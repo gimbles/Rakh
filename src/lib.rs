5a8f02efb65f4a36f349f90417fb4d1a61af3a94
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2021-12-22 08:56:14 -0500

    Added tests and doctests
    Added a test and doctest for interperet()

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,13 @@ pub enum Error {
     NoKeyGiven,
 }
 
+/// # Examples
+///
+/// configuration lines must contain a ':' and use a space as a delimiter
+/// ```
+/// let config = crate::rakh::interpret("deno_is_uncool:false\nname_of_funny_crab:Ferris".to_string()).unwrap();
+/// assert_eq!(config.get("deno_is_uncool").unwrap(), "false");
+/// ```
 pub fn interpret(code: String) -> Result<HashMap<String, String>, Error> {
     let lines = code.split('\n');
     let mut hashmap: HashMap<String, String> = HashMap::new();
@@ -23,4 +30,23 @@ pub fn interpret(code: String) -> Result<HashMap<String, String>, Error> {
     }
 
     Ok(hashmap)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::interpret;
+
+    #[test]
+    fn parsing_works() {
+        let config = interpret(
+            "rust_is_awesome:true\ndeno_is_uncool:false\nname_of_funny_crab:Ferris".to_string(),
+        )
+        .unwrap();
+        assert_eq!(config.get("rust_is_awesome"), Some(&"true".to_string()));
+        assert_eq!(config.get("deno_is_uncool"), Some(&"false".to_string()));
+        assert_eq!(
+            config.get("name_of_funny_crab"),
+            Some(&"Ferris".to_string())
+        );
+    }
 }

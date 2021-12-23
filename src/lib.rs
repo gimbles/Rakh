5b31bb05ee6b07dbf537c7747651ad6456d2bcac
Yush08 <yusharora@protonmail.com>
2021-12-23 10:00:19 +0530

    Manually merge other PRs chloe did

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ pub enum Error {
     NoKeyGiven,
 }
 
-pub fn interpret(code: String) -> Result<HashMap<String, String>, Error> {
+pub fn interpret(code: &str) -> Result<HashMap<&str, &str>, Error> {
     let lines = code.split('\n');
-    let mut hashmap: HashMap<String, String> = HashMap::new();
+    let mut hashmap: HashMap<&str, &str> = HashMap::new();
 
     for line in lines {
         let key_value = line.split_once(':');
@@ -18,9 +18,25 @@ pub fn interpret(code: String) -> Result<HashMap<String, String>, Error> {
             if key_value.0.is_empty() {
                 return Err(Error::NoKeyGiven);
             }
-            hashmap.insert(key_value.0.to_string(), key_value.1.to_string());
+            hashmap.insert(key_value.0, key_value.1);
         }
     }
 
     Ok(hashmap)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::interpret;
+
+    #[test]
+    fn parsing_works() {
+        let config = interpret("rust_is_awesome:true\ndeno_is_uncool:false\nname_of_cute_crab:Ferris!"
+        )
+        .unwrap();
+        dbg!(&config);
+        assert_eq!(config.get("rust_is_awesome").unwrap(), &"true");
+        assert_eq!(config.get("deno_is_uncool").unwrap(), &"false");
+        assert_eq!(config.get("name_of_cute_crab").unwrap(), &"Ferris!");
+    }
 }

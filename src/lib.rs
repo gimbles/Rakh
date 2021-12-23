18fdfe928ee53ff4639bc33f829ed1c570fa5d37
Yush08 <yusharora@protonmail.com>
2021-12-23 10:00:53 +0530

    chore: Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -31,9 +31,9 @@ mod tests {
 
     #[test]
     fn parsing_works() {
-        let config = interpret("rust_is_awesome:true\ndeno_is_uncool:false\nname_of_cute_crab:Ferris!"
-        )
-        .unwrap();
+        let config =
+            interpret("rust_is_awesome:true\ndeno_is_uncool:false\nname_of_cute_crab:Ferris!")
+                .unwrap();
         dbg!(&config);
         assert_eq!(config.get("rust_is_awesome").unwrap(), &"true");
         assert_eq!(config.get("deno_is_uncool").unwrap(), &"false");

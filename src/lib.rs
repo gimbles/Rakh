c7eec009222b8304286a8ebc23fc00f7cbc05636
Gitea <gitea@fake.local>
2022-01-04 17:59:23 +0530

    feat: Support keys with colons

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,22 @@ pub enum Error {
     NoKeyGiven,
 }
 
-pub fn interpret(code: &str) -> Result<HashMap<&str, &str>, Error> {
+pub fn interpret(code: &str) -> Result<HashMap<String, &str>, Error> {
     let lines = code.split('\n');
-    let mut hashmap: HashMap<&str, &str> = HashMap::new();
+    let mut hashmap: HashMap<String, &str> = HashMap::new();
 
     for line in lines {
-        let key_value = line.split_once(':');
+        let mut key_value = line.split(":").collect::<Vec<_>>();
 
-        if let Some(key_value) = key_value {
-            if key_value.0.is_empty() {
-                return Err(Error::NoKeyGiven);
-            }
-            hashmap.insert(key_value.0, key_value.1);
+        let value = key_value[key_value.len() - 1];
+        key_value.remove(key_value.len() - 1);
+        let key = key_value.join(":");
+
+        if key_value[0].is_empty() {
+            return Err(Error::NoKeyGiven);
         }
+
+        hashmap.insert(key, value);
     }
 
     Ok(hashmap)
@@ -34,9 +37,21 @@ mod tests {
         let config =
             interpret("rust_is_awesome:true\ndeno_is_uncool:false\nname_of_cute_crab:Ferris!")
                 .unwrap();
-                
+
         assert_eq!(config.get("rust_is_awesome").unwrap(), &"true");
         assert_eq!(config.get("deno_is_uncool").unwrap(), &"false");
         assert_eq!(config.get("name_of_cute_crab").unwrap(), &"Ferris!");
+    }
+
+    #[test]
+    fn colon_in_key() {
+        let config =
+            interpret("this:key:has:a:colon:in:its:keyname:THIS IS THE ACTUAL VALUE OF THE KEY")
+                .unwrap();
+
+        assert_eq!(
+            config.get("this:key:has:a:colon:in:its:keyname").unwrap(),
+            &"THIS IS THE ACTUAL VALUE OF THE KEY"
+        );
     }
 }

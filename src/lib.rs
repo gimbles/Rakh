d0a21ae50fd9b46a50a08f94142f25153c6cf2f8
Yush08 <yusharora@protonmail.com>
2022-01-04 23:30:29 +0530

    keys can no longer have colons

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub fn interpret(code: &str) -> Result<HashMap<&str, &str>, Error> {
     let mut hashmap: HashMap<&str, &str> = HashMap::new();
 
     for line in code.lines() {
-        match line.rsplit_once(':') {
+        match line.split_once(':') {
             Some((key, value)) => {
                 if key.is_empty() {
                     return Err(Error::NoKeyGiven);
@@ -42,17 +42,5 @@ mod tests {
         assert_eq!(config.get("rust_is_awesome").unwrap(), &"true");
         assert_eq!(config.get("deno_is_uncool").unwrap(), &"false");
         assert_eq!(config.get("name_of_cute_crab").unwrap(), &"Ferris!");
-    }
-
-    #[test]
-    fn colon_in_key() {
-        let config =
-            interpret("this:key:has:a:colon:in:its:keyname:THIS IS THE ACTUAL VALUE OF THE KEY")
-                .unwrap();
-
-        assert_eq!(
-            config.get("this:key:has:a:colon:in:its:keyname").unwrap(),
-            &"THIS IS THE ACTUAL VALUE OF THE KEY"
-        );
     }
 }

a0eae66be2d4ec10f5d64ac207a89e8b3c6df4d9
Gimbles <93856041+gimbles@users.noreply.github.com>
2022-02-24 12:43:38 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub enum Error {
     NoKeyGiven,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Types {
     String(String),
     Integer(i64),
@@ -17,16 +17,14 @@ pub enum Types {
     Object(HashMap<String, Types>),
 }
 
-
-pub fn interpret(code: &str) ->  Result<HashMap<&str, Types>, Error> {
-    let mut hashmap: HashMap<&str,Types> = HashMap::new();
+pub fn interpret(code: &str) -> Result<HashMap<&str, Types>, Error> {
+    let mut hashmap: HashMap<&str, Types> = HashMap::new();
     for line in code.lines() {
         if line.is_empty() {
             continue;
         };
         match line.split_once(':') {
             Some((key, value)) => {
-
                 if key.is_empty() {
                     panic!("No key given");
                 }
@@ -34,21 +32,21 @@ pub fn interpret(code: &str) ->  Result<HashMap<&str, Types>, Error> {
                     hashmap.insert(key, Types::String(value.to_string().replace("\"", "")));
                 } else {
                     let new_value = value.parse::<i64>();
-                    if new_value.is_ok() {
-                        hashmap.insert(key, Types::Integer(new_value.unwrap()));
+                    if let Ok(nv) = new_value {
+                        hashmap.insert(key, Types::Integer(nv));
                     } else {
                         let float_value = value.parse::<f64>();
-                        if float_value.is_ok() {
-                            hashmap.insert(key, Types::Float(float_value.unwrap()));
+                        if let Ok(nv) = float_value {
+                            hashmap.insert(key, Types::Float(nv));
                         } else {
                             let bool_value = value.parse::<bool>();
-                            if bool_value.is_ok() {
-                                hashmap.insert(key, Types::Boolean(bool_value.unwrap()));
+                            if let Ok(nv) = bool_value {
+                                hashmap.insert(key, Types::Boolean(nv));
                             }
                         }
                     }
                 }
-            },
+            }
             None => {
                 panic!("No key given");
             }
@@ -67,11 +65,23 @@ mod tests {
         let config =
             interpret("hello:\"world\"\nrust_is_awesome:true\nrust_version:1.58\nrust_is_number:1\ndeno_is_uncool:false\nname_of_cute_crab:\"Ferris!\"")
                 .unwrap();
-        assert_eq!(config.get("rust_is_awesome").unwrap(), &Types::Boolean(true));
+        assert_eq!(
+            config.get("rust_is_awesome").unwrap(),
+            &Types::Boolean(true)
+        );
         assert_eq!(config.get("rust_version").unwrap(), &Types::Float(1.58));
         assert_eq!(config.get("rust_is_number").unwrap(), &Types::Integer(1));
-        assert_eq!(config.get("deno_is_uncool").unwrap(), &Types::Boolean(false));
-        assert_eq!(config.get("name_of_cute_crab").unwrap(), &Types::String("Ferris!".to_string()));
-        assert_eq!(config.get("hello").unwrap(), &Types::String("world".to_string()));
+        assert_eq!(
+            config.get("deno_is_uncool").unwrap(),
+            &Types::Boolean(false)
+        );
+        assert_eq!(
+            config.get("name_of_cute_crab").unwrap(),
+            &Types::String("Ferris!".to_string())
+        );
+        assert_eq!(
+            config.get("hello").unwrap(),
+            &Types::String("world".to_string())
+        );
     }
 }
